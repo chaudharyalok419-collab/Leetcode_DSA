cbc5641a401c9b542b3938d237891c38de1c9f6e
Alok Chaudhary <chaudharyalok419@gmail.com>
2026-07-02 20:08:03 +0530

    Time: 167 ms (14.48%) | Memory: 66.8 MB (13.71%) - LeetSync

--- 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
+++ 3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -2,46 +2,44 @@ class Solution {
 public:
     int m, n;
     vector<vector<int>> dir{{1,0},{-1,0},{0,1},{0,-1}};
-    vector<vector<vector<int>>>dp;
-    bool issafe(vector<vector<int>>& grid, int h, int i, int j)
-    {
-        if (grid[i][j] == 1)
-            h--;
-
-        if (h <= 0)
-            return false;
-
-        if (i == m-1 && j == n-1)
-            return true;
-            if (dp[i][j][h]!=-1)
-               return dp[i][j][h];
-
-        int temp = grid[i][j];
-        grid[i][j] = 2;
-
-        for (auto it : dir)
-        {
-            int I = i + it[0];
-            int J = j + it[1];
-
-            if (I >= 0 && I < m && J >= 0 && J < n && grid[I][J] != 2)
-            {
-                if (issafe(grid, h, I, J))
-                    return  dp[i][j][h]=true;
-            }
-        }
-
-        grid[i][j] = temp;
-
-        return dp[i][j][h]=false;
-    }
-
     bool findSafeWalk(vector<vector<int>>& grid, int h)
-    {   
-        m = grid.size();
-        n = grid[0].size();
-        dp.resize(m,vector<vector<int>>(n,vector<int>(h+1,-1)));
+    {      
 
-        return issafe(grid, h, 0, 0);
+            n=grid.size();
+            m= grid[0].size();
+            vector<vector<int>>ans(n,vector<int>(m,INT_MAX)); 
+            queue<pair<int,int>>q;
+            ans[0][0]=grid[0][0];
+            q.push({0,0});
+            while (!q.empty())
+            {
+                auto it = q.front();
+                q.pop();
+                int x= it.first;
+                int y= it.second;
+                for (auto t: dir)
+                {
+                    int i= t[0]+x;
+                    int j= t[1]+y;
+                    if (i>=0 && i<n && j>=0 && j<m)
+                    {                               
+                       int a= ans[x][y]+grid[i][j];
+                       if (ans[i][j]>a)
+                       {
+                        ans[i][j]=a;
+                        q.push({i,j});
+                       }
+                     
+
+                    }
+                }
+            }
+
+
+
+
+        return (h>ans[n-1][m-1]);
+        
+        
     }
 };
