d472b1251db9974054bc14722e09370897794cf2
Alok Chaudhary <chaudharyalok419@gmail.com>
2026-07-04 11:15:46 +0530

    Time: 49 ms (85.49%) | Memory: 132.8 MB (67.96%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
-        unordered_map<int,vector<vector<int>>>mpp;
-    
-      for (auto it: roads )
-       {
-          int u= it[0];
-          int v= it[1];
-          int w= it [2];
-          mpp[u].push_back({v,w});
-          mpp[v].push_back({u,w});
+        vector<vector<pair<int, int>>> adj(n + 1);
 
-       }
-        vector<bool>vis(n+1,0);
-        int ans=INT_MAX;
-        queue<int>q;
-       q.push(1);
-       while (!q.empty())
-       { int a= q.front() ;
-          q.pop();
-          vis[a]=1;
-          for (auto it: mpp[a])
-            {
-                int v= it[0];
-                int wt= it[1];
-                ans=min(ans,wt);
-                 if (!vis[v])
-                 q.push(v);
+        for (auto &road : roads) {
+            int u = road[0];
+            int v = road[1];
+            int w = road[2];
+
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
+        }
+
+        vector<bool> vis(n + 1, false);
+        queue<int> q;
+
+        q.push(1);
+        vis[1] = true;
+
+        int ans = INT_MAX;
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+
+            for (auto &[v, wt] : adj[u]) {
+                ans = min(ans, wt);
+
+                if (!vis[v]) {
+                    vis[v] = true;
+                    q.push(v);
+                }
             }
-       }
-       return ans;
-        
+        }
+
+        return ans;
     }
 };
