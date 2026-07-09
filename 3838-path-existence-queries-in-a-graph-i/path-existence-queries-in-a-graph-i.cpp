aaf5516aa8ffac9efe38d13f329081e4054a853f
Alok Chaudhary <chaudharyalok419@gmail.com>
2026-07-09 21:32:25 +0530

    Time: 54 ms (28.77%) | Memory: 235.9 MB (24.93%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -1,59 +1,27 @@
 class Solution {
 public:
-    vector<int> parent, rank;
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+      vector<int>grp(n,0);
+      int cg =0;
+       for (int i=1;i<n;i++)
+         {
+            if (nums[i]-nums[i-1]>maxDiff)
+              {
+                cg++;
+              }
+              grp[i]=cg;
+         }
+          vector<bool>ans;
+           for (auto it: queries )
+           {
+            int u= it[0];
+            int v= it[1];
+            if (grp[v]==grp[u])
+            ans.push_back(1);
+            else 
+            ans.push_back(0);
 
-    int find(int x) {
-        if (parent[x] == x)
-            return x;
-        return parent[x] = find(parent[x]);
-    }
-
-    void unite(int x, int y) {
-        int px = find(x);
-        int py = find(y);
-
-        if (px == py)
-            return;
-
-        if (rank[px] < rank[py])
-            parent[px] = py;
-        else if (rank[px] > rank[py])
-            parent[py] = px;
-        else {
-            parent[py] = px;
-            rank[px]++;
-        }
-    }
-
-    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int mD,
-                                      vector<vector<int>>& queries) {
-
-        parent.resize(n);
-        rank.assign(n, 0);
-
-        for (int i = 0; i < n; i++)
-            parent[i] = i;
-
-       
-        vector<pair<int, int>> arr;
-        for (int i = 0; i < n; i++)
-            arr.push_back({nums[i], i});
-
-        sort(arr.begin(), arr.end());
-
-        
-        for (int i = 1; i < n; i++) {
-            if (arr[i].first - arr[i - 1].first <= mD) {
-                unite(arr[i].second, arr[i - 1].second);
-            }
-        }
-
-        vector<bool> ans;
-
-        for (auto &q : queries) {
-            ans.push_back(find(q[0]) == find(q[1]));
-        }
-
-        return ans;
+           }
+           return ans;
     }
 };
