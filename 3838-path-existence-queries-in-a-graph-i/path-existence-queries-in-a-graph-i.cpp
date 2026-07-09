eefee1d55fbdfa455893e51e96e0c787a5455dfc
Alok Chaudhary <chaudharyalok419@gmail.com>
2026-07-09 21:21:50 +0530

    Time: 80 ms (16.44%) | Memory: 252.3 MB (9.59%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -1,29 +1,41 @@
 class Solution {
 public:
-    vector<int> visited;
+    vector<int> parent, rank;
 
-    void bfs(queue<int>& q, int a, unordered_map<int, vector<int>>& mp) {
-        while (!q.empty()) {
-            int u = q.front();
-            q.pop();
+    int find(int x) {
+        if (parent[x] == x)
+            return x;
+        return parent[x] = find(parent[x]);
+    }
 
-            for (auto &it : mp[u]) {
-                if (visited[it] == -1) {
-                    visited[it] = a;
-                    q.push(it);
-                }
-            }
+    void unite(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+
+        if (px == py)
+            return;
+
+        if (rank[px] < rank[py])
+            parent[px] = py;
+        else if (rank[px] > rank[py])
+            parent[py] = px;
+        else {
+            parent[py] = px;
+            rank[px]++;
         }
     }
 
-    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int mD, vector<vector<int>>& queries) {
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int mD,
+                                      vector<vector<int>>& queries) {
 
-        visited.assign(n, -1);
-        unordered_map<int, vector<int>> mp;
-        vector<bool> ans;
+        parent.resize(n);
+        rank.assign(n, 0);
+
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
 
        
-        vector<pair<int,int>> arr;
+        vector<pair<int, int>> arr;
         for (int i = 0; i < n; i++)
             arr.push_back({nums[i], i});
 
@@ -32,28 +44,14 @@ public:
         
         for (int i = 1; i < n; i++) {
             if (arr[i].first - arr[i - 1].first <= mD) {
-                int u = arr[i].second;
-                int v = arr[i - 1].second;
-
-                mp[u].push_back(v);
-                mp[v].push_back(u);
+                unite(arr[i].second, arr[i - 1].second);
             }
         }
 
-        queue<int> q;
-        int comp = 0;
+        vector<bool> ans;
 
-        for (int i = 0; i < n; i++) {
-            if (visited[i] == -1) {
-                visited[i] = comp;
-                q.push(i);
-                bfs(q, comp, mp);
-                comp++;
-            }
-        }
-
-        for (auto &it : queries) {
-            ans.push_back(visited[it[0]] == visited[it[1]]);
+        for (auto &q : queries) {
+            ans.push_back(find(q[0]) == find(q[1]));
         }
 
         return ans;
