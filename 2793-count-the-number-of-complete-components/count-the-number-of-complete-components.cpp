beb18f418603cb7238ada231788b66f791d06429
Alok Chaudhary <chaudharyalok419@gmail.com>
2026-07-11 18:27:26 +0530

    Time: 59 ms (39.60%) | Memory: 134.9 MB (44.53%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,47 +1,45 @@
 class Solution {
 public:
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n);
-
-        for (auto &e : edges) {
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
+        vector<bool>visited(n,0);
+        unordered_map<int,vector<int>>mpp;
+        for (auto & it: edges )
+        {
+            int u =it[0];
+            int v= it[1];
+            mpp[u].push_back(v);
+            mpp[v].push_back(u);
         }
+        queue<int>q;
+        int ans=0;
+        for (int i=0;i<n;i++)
+        {
+            if (!visited[i])
+              {  
+                q.push(i);
+                  visited[i]=1;
+                 int nodes=0;
+                 int edges=0;
+                while (!q.empty())
+                {
+                    int a= q.front();
+                       q.pop();
+                      nodes++;
+                      edges+=mpp[a].size();
+                     for (auto & it :mpp[a])
+                     {    if (!visited[it])
+                            {
+                            visited[it]=1;
+                            q.push(it);
+                             }
 
-        vector<bool> vis(n, false);
-        int ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (vis[i]) continue;
-
-            queue<int> q;
-            q.push(i);
-            vis[i] = true;
-
-            int nodes = 0;
-            int edgeCount = 0;
-
-            while (!q.empty()) {
-                int u = q.front();
-                q.pop();
-
-                nodes++;
-                edgeCount += adj[u].size();
-
-                for (int v : adj[u]) {
-                    if (!vis[v]) {
-                        vis[v] = true;
-                        q.push(v);
-                    }
-                }
-            }
-
-            edgeCount /= 2;
-
-            if (edgeCount == nodes * (nodes - 1) / 2)
-                ans++;
+                     }
+                }   edges/=2;
+                if (edges==(nodes*(nodes-1)/2))
+                   ans++;
+              }
         }
-
         return ans;
+        
     }
 };
