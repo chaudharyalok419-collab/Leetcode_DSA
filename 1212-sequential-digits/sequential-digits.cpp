ccdf6086244fef056d25c59c5417e0d3068ab70a
Alok Chaudhary <chaudharyalok419@gmail.com>
2026-07-13 18:28:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (76.91%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -3,14 +3,17 @@ public:
     vector<int> sequentialDigits(int low, int high) {
         vector<int> ans;
         string s = "123456789";
+          for (int len=2;len<=9;len++)
+           {
+              for (int i=0;i+len<=9;i++)
+                {   string t= string(s.begin()+i,s.begin()+i+len);
+                    int num= stoi(t);
+                     
+                    if (num>=low && num<=high)
+                       ans.push_back(num);
 
-        for (int len = 2; len <= 9; len++) {
-            for (int i = 0; i + len <= 9; i++) {
-                int num = stoi(s.substr(i, len));
-                if (num >= low && num <= high)
-                    ans.push_back(num);
-            }
-        }
+                }
+           }
 
         return ans;
     }
