bcde301eacf0419bc09c133f5e1885490aadf473
Alok Chaudhary <chaudharyalok419@gmail.com>
2026-09-03 22:45:13 +0530

    Time: 5 ms (54.71%) | Memory: 165.8 MB (87.43%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -4,14 +4,10 @@ public:
         int a=*min_element(nums.begin(),nums.end());
         int t=0;
         for (int i : nums)
-        {
-           if (i&1)
-              t=1;
-        }
+        if (i&1)t=1;
+    
         if (t && a%2==0)
            return false ;
-        
-
         return true;
       
     }
