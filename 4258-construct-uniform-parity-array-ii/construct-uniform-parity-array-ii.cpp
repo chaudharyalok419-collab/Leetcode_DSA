a1ac702be17369d7bb5c23a9af41c1595b9604c5
Alok Chaudhary <chaudharyalok419@gmail.com>
2026-09-03 22:36:39 +0530

    Time: 0 ms (100.00%) | Memory: 165.8 MB (62.83%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums) {
+        int a=*min_element(nums.begin(),nums.end());
+        int t=0;
+        for (int i : nums)
+        {
+           if (i&1)
+              t=1;
+        }
+        if (t && a%2==0)
+           return false ;
+        
+
+        return true;
+      
+    }
+};
