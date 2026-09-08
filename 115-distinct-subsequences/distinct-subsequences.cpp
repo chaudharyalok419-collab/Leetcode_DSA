75c48e94718868b99724e93db4aa12b31e078d7a
Alok Chaudhary <chaudharyalok419@gmail.com>
2026-09-08 06:38:04 +0530

    Time: 30 ms (54.68%) | Memory: 27.1 MB (43.27%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-
-    int  helper(string &s, string &t, int i, int j)
-    {
-       
-        if (j == t.size())
+  vector<vector<int>>dp;
+   int helper( string &s,string &t , int i, int j )
+     {
+        if (j<0)
             return 1;
+             if (i<0)
+               return 0;
+               if (dp[i+1][j+1]!=-1)
+                 return dp[i+1][j+1];
+                  
 
-        if (i == s.size())
-            return 0;
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        int skip = helper(s, t, i + 1, j);
-
-        int  take = 0;
-        if (s[i] == t[j])
-            take = helper(s, t, i + 1, j + 1);
-
-        return dp[i][j] = take + skip;
-    }
-
+           int skip=helper (s ,t ,i-1,j);
+           int take=0;
+           if (s[i]==t[j])
+              take = helper(s,t,i-1,j-1);
+             return dp[i+1][j+1]=take+skip;
+     }
     int numDistinct(string s, string t) {
-        dp.assign(s.size(), vector<int>(t.size(), -1));
-        return helper(s, t, 0, 0);
+         dp.resize(s.size()+1,vector<int>(t.size()+1,-1));
+        return helper(s,t,s.size()-1,t.size()-1);
+        
     }
 };
