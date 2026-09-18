6c0e84f4b32cbaecb901c89a6f0776b2c5f6053a
Alok Chaudhary <chaudharyalok419@gmail.com>
2026-09-19 00:27:52 +0530

    Time: 15 ms (85.14%) | Memory: 12.5 MB (67.16%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,18 +1,13 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        map={}
+        map=[0]*26
         for c in s :
-            if c not in map:
-               map[c]=1
-            else:
-                map[c]+=1
+            map[ord(c)-ord('a')]+=1
         for c in t:
-            if c not in map:
-                return False
-            else:
-                 map[c]-=1
+             map[ord(c)-ord('a')]-=1
+               
         for x in map:
-          if map[x]!=0:
+          if x!=0:
              return False
         return True
         
